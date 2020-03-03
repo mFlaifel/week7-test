de85df9811b05bb9b99b6860bc300a300267f2b0
mFlaifel <msf-2oo7@hotmail.com>
2020-03-03 22:03:17 +0200

    submit hw

--- server/database/queries/addCity.js
+++ server/database/queries/addCity.js
@@ -6,7 +6,7 @@ const addCity = (data) => {
     name, city,
   } = data;
   const sql = {
-    text: 'INSERT INTO recipe (name, ingredients, preparation, imgurl) VALUES ($1, $2);',
+    text: 'INSERT INTO cities (name,city) VALUES ($1, $2);',
     values: [name, city],
   };
   return Connection.query(sql);
